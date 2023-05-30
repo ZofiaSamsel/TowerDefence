6cadd03dde6a4bea0f5bfdaf51346a70ee65278a
ZofiaSamsel <zosiasamsel@gmail.com>
2023-05-30 18:52:35 +0200

    ZAJECIA 30/05

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -1,10 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 
 public class LevelManager : MonoBehaviour
 {
-    [SerializeField] GameObject kafelek;
+    [SerializeField] GameObject[] kafelek;
     public float rozmiarKafelka;
     
     // Start is called before the first frame update
@@ -13,25 +14,39 @@ public class LevelManager : MonoBehaviour
         StworzPoziom();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     private void StworzPoziom()
     {
-        rozmiarKafelka = kafelek.GetComponent<SpriteRenderer>().sprite.bounds.size.x;
+        string[] daneMapy = new string[]
+        {
+            "00000",
+            "11111",
+            "10101",
+            "00000",
+            "10101",
+            "11111",
+        };
+
+        rozmiarKafelka = kafelek[0].GetComponent<SpriteRenderer>().sprite.bounds.size.x;
         Vector2 LewyKamery =  Camera.main.ScreenToWorldPoint(new Vector2(0, 0));
+
+
         for (int y = 0; y < 5; y++)
         {
+            char[] noweKafelki = daneMapy[y].ToCharArray();
+
             for (int x = 0; x < 5; x++)
             {
-
-                Instantiate(kafelek, new Vector2( LewyKamery.x + rozmiarKafelka * x, LewyKamery.y + rozmiarKafelka * y), Quaternion.identity);
-
+                UmiescKafelek(noweKafelki[x], rozmiarKafelka, LewyKamery, x, y);
+                
 
             }
         }
+    }
+    private void UmiescKafelek(char rodzajKafelka, float rozmiarKafelka, Vector2 LewyKamery, int x, int y)
+    {
+        int numerKafelka = (int)System.Char.GetNumericValue(rodzajKafelka);
+
+        Instantiate(kafelek[numerKafelka], new Vector2(LewyKamery.x + rozmiarKafelka * x, LewyKamery.y + rozmiarKafelka * y), Quaternion.identity);
+
     }
 }
